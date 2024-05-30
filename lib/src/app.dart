212374b90f52361ded1f841a366e8559949dbc4c
Samuel <samuelketema01@gmail.com>
2024-05-30 22:19:38 +0300

    Root widget

---
+++ lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'Screens/imput_page.dart';
+
+class MyApp extends StatelessWidget{
+  Widget build(BuildContext context){
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      title:'Bmi Calculator',
+      home:ImputPage()
+    );
+  }
+}
